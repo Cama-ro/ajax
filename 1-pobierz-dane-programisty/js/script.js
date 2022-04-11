a88d8e83cff20af3487259ee49ee4fe677e9846c
Cama-ro <kam.rybicka@gmail.com>
2022-04-11 13:09:23 +0200

    pobieranie danych programisty - jquery i ajax

--- 1-pobierz-dane-programisty/js/script.js
+++ 1-pobierz-dane-programisty/js/script.js
@@ -1,8 +1,24 @@
 $(document).ready(function () {
   $(".button").click(function () {
-    $.getJSON("https://jsonplaceholder.typicode.com/users/1", function (data) {
-      console.log(data);
-      $("#dane-programisty").text(JSON.stringify(data));
+    $.getJSON(
+      "https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php",
+      function (data) {
+        console.log(data);
+        // 1a. create p elements in jquery by $('<selector></selector>')
+        // 1b. add text inner created elements by .text()
+        let pName = $("<p></p>").text(data.imie);
+        let pSurname = $("<p></p>").text(data.nazwisko);
+        let pJob = $("<p></p>").text(data.zawod);
+        let pCompany = $("<p></p>").text(data.firma);
+
+        // add elements to html by append, inner div id="dane-programisty"
+        $("#dane-programisty").append(pName);
+        $("#dane-programisty").append(pSurname);
+        $("#dane-programisty").append(pJob);
+        $("#dane-programisty").append(pCompany);
+      }
+    ).fail(function (error) {
+      console.error(error);
     });
   });
 });
